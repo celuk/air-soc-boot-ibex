b14cbac7804737d71161f7f47b561d9b2580fb62
celuk <seyyid4091@gmail.com>
2024-05-21 22:54:37 +0300

    feat: connect cv32e40p ports

---
+++ rtl/header.vh
@@ -0,0 +1,17 @@
+`define COREV_PULP       1'b0
+`define COREV_CLUSTER    1'b0
+`define FPU              1'b0
+`define FPU_ADDMUL_LAT   1'b0
+`define FPU_OTHERS_LAT   1'b0
+`define ZFINX            1'b0
+`define NUM_MHPMCOUNTERS 1'b1  
+
+`define BOOT_ADDR 32'h40000000
+`define MTVEC_ADDR 32'h0
+`define DM_HALT_ADDR 32'h1A110800
+
+`define PULP_CLOCK_EN 1'b0
+`define SCAN_CG_EN 1'b0
+
+`define HART_ID 32'h0
+`define DM_EXCEPTION_ADDR 32'h0
--- rtl/src/air_soc.sv
+++ rtl/src/air_soc.sv
@@ -4,42 +4,92 @@
 `include "header.vh"
 
 module air_soc (
-   input wire clk_i,
-   input wire rst_ni,
-   
-   output wire        iomem_valid,
-   input  wire        iomem_ready,
-   output wire [ 3:0] iomem_wstrb,
-   output wire [31:0] iomem_addr,
-   output wire [31:0] iomem_wdata,
-   input  wire [31:0] iomem_rdata,
-   
-   output wire uart_tx_o,
-   input  wire uart_rx_i,
-   
-   output wire spi_cs_o,
-   output wire spi_sck_o,
-   output wire spi_mosi_o,
-   input  wire spi_miso_i,
-   
-   output wire pwm0_o,
-   output wire pwm1_o
+    input wire clk_i,
+    input wire rst_ni,
+    
+    output wire        iomem_valid,
+    input  wire        iomem_ready,
+    output wire [ 3:0] iomem_wstrb,
+    output wire [31:0] iomem_addr,
+    output wire [31:0] iomem_wdata,
+    input  wire [31:0] iomem_rdata,
+    
+    output wire uart_tx_o,
+    input  wire uart_rx_i,
+    
+    output wire spi_cs_o,
+    output wire spi_sck_o,
+    output wire spi_mosi_o,
+    input  wire spi_miso_i,
+    
+    output wire pwm0_o,
+    output wire pwm1_o
 
 );
 
-cv32e40p_top cv32e40p_core_ip #(
-   parameter COREV_PULP = 0,
-   parameter COREV_CLUSTER = 0,
-   parameter FPU = 0,
-   parameter FPU_ADDMUL_LAT = 0,
-   parameter FPU_OTHERS_LAT = 0,
-   parameter ZFINX = 0,
-   parameter NUM_MHPMCOUNTERS = 1
+wire       data_we_o;
+wire [3:0] data_be_o;
+
+assign iomem_wstrb = data_we_o ? data_be_o : 4'h0;
+
+cv32e40p_top #(
+    .COREV_PULP               ( `COREV_PULP ),
+    .COREV_CLUSTER            ( `COREV_CLUSTER ),
+    .FPU                      ( `FPU ),
+    .FPU_ADDMUL_LAT           ( `FPU_ADDMUL_LAT ),
+    .FPU_OTHERS_LAT           ( `FPU_OTHERS_LAT ),
+    .ZFINX                    ( `ZFINX ),
+    .NUM_MHPMCOUNTERS         ( `NUM_MHPMCOUNTERS )
 )
-(
-   .clk_i (clk_i),
-   .rst_ni(rst_ni)
-   
+cv32e40p_core_ip (
+    .clk_i                    (clk_i),
+    .rst_ni                   (rst_ni),
+
+    .pulp_clock_en_i          (`PULP_CLOCK_EN), // PULP clock enable (only used if COREV_CLUSTER = 1)
+    .scan_cg_en_i             (`SCAN_CG_EN), // Enable all clock gates for testing
+
+    // Configuration
+    .boot_addr_i              (`BOOT_ADDR),
+    .mtvec_addr_i             (`MTVEC_ADDR),
+    .dm_halt_addr_i           (`DM_HALT_ADDR),
+    .hart_id_i                (`HART_ID),
+    .dm_exception_addr_i      (`DM_EXCEPTION_ADDR),
+    
+    // TODO: Always valid?
+    // Instruction memory interface
+    .instr_req_o              (iomem_valid),
+    .instr_gnt_i              (iomem_ready),
+    .instr_rvalid_i           (1'b1),
+    .instr_addr_o             (iomem_addr),
+    .instr_rdata_i            (iomem_rdata),
+
+    // TODO: Always valid?
+    // Data memory interface
+    .data_req_o               (iomem_valid),
+    .data_gnt_i               (iomem_ready),
+    .data_rvalid_i            (1'b1),
+    .data_we_o                (data_we_o),
+    .data_be_o                (data_be_o),
+    .data_addr_o              (iomem_addr),
+    .data_wdata_o             (iomem_wdata),
+    .data_rdata_i             (iomem_rdata),
+
+    // Interrupt interface
+    .irq_i                    (32'h0),
+    .irq_ack_o                (),
+    .irq_id_o                 (),
+
+    // TODO: JTAG Integration
+    // Debug interface
+    .debug_req_i              (1'b0),
+    .debug_havereset_o        (),
+    .debug_running_o          (),
+    .debug_halted_o           ()
+
+    // TODO
+    // CPU Control Signals
+    .fetch_enable_i           (1'b1),
+    .core_sleep_o             ()
 );
 
 
