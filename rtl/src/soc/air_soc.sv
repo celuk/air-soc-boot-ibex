3dfad8adab27c20eaf64dcca1575854c1ae53fd6
hakan-demirli <ehdemirli@gmail.com>
2024-08-12 01:41:36 +0300

    fix: simplify

--- rtl/src/soc/air_soc.sv
+++ rtl/src/soc/air_soc.sv
@@ -1,8 +1,6 @@
 // air_soc.sv
 `timescale 1ns / 1ps
-
-`include "header.vh"
-
+//
 `default_nettype none
 
 module air_soc #(
@@ -56,14 +54,60 @@ module air_soc #(
    logic               cache_rvalid;
    logic [MEM_W  -1:0] cache_rdata;
 
-   logic               periph_req;
-   logic [       31:0] periph_addr;
-   logic               periph_we;
-   logic [MEM_W/8-1:0] periph_be;
-   logic [MEM_W  -1:0] periph_wdata;
-   logic               periph_gnt;
-   logic               periph_rvalid;
-   logic [MEM_W  -1:0] periph_rdata;
+   logic               uart_req;
+   logic [       31:0] uart_addr;
+   logic               uart_we;
+   logic [MEM_W/8-1:0] uart_be;
+   logic [MEM_W  -1:0] uart_wdata;
+   logic               uart_gnt;
+   logic               uart_rvalid;
+   logic [MEM_W  -1:0] uart_rdata;
+
+   logic               timer_req;
+   logic [       31:0] timer_addr;
+   logic               timer_we;
+   logic [MEM_W/8-1:0] timer_be;
+   logic [MEM_W  -1:0] timer_wdata;
+   logic               timer_gnt;
+   logic               timer_rvalid;
+   logic [MEM_W  -1:0] timer_rdata;
+
+   logic               gpio_req;
+   logic [       31:0] gpio_addr;
+   logic               gpio_we;
+   logic [MEM_W/8-1:0] gpio_be;
+   logic [MEM_W  -1:0] gpio_wdata;
+   logic               gpio_gnt;
+   logic               gpio_rvalid;
+   logic [MEM_W  -1:0] gpio_rdata;
+
+   logic               spi_req;
+   logic [       31:0] spi_addr;
+   logic               spi_we;
+   logic [MEM_W/8-1:0] spi_be;
+   logic [MEM_W  -1:0] spi_wdata;
+   logic               spi_gnt;
+   logic               spi_rvalid;
+   logic [MEM_W  -1:0] spi_rdata;
+
+   logic               i2c_req;
+   logic [       31:0] i2c_addr;
+   logic               i2c_we;
+   logic [MEM_W/8-1:0] i2c_be;
+   logic [MEM_W  -1:0] i2c_wdata;
+   logic               i2c_gnt;
+   logic               i2c_rvalid;
+   logic [MEM_W  -1:0] i2c_rdata;
+
+   logic               usb_req;
+   logic [       31:0] usb_addr;
+   logic               usb_we;
+   logic [MEM_W/8-1:0] usb_be;
+   logic [MEM_W  -1:0] usb_wdata;
+   logic               usb_gnt;
+   logic               usb_rvalid;
+   logic [MEM_W  -1:0] usb_rdata;
+
 
    // instruction cache
    logic               imem_req;
@@ -72,76 +116,64 @@ module air_soc #(
    logic               imem_rvalid;
    logic [  MEM_W-1:0] imem_rdata;
 
-   // data cache
-   logic               dmem_req;
-   logic               dmem_gnt;
-   logic [       31:0] dmem_addr;
-   logic               dmem_we;
-   logic [MEM_W  -1:0] dmem_wdata;
-   logic               dmem_rvalid;
-   logic               dmem_wvalid;
-   logic [MEM_W  -1:0] dmem_rdata;
 
    cv32e40p_top #(
-       .COREV_PULP               ( `COREV_PULP ),
-       .COREV_CLUSTER            ( `COREV_CLUSTER ),
-       .FPU                      ( `FPU ),
-       .FPU_ADDMUL_LAT           ( `FPU_ADDMUL_LAT ),
-       .FPU_OTHERS_LAT           ( `FPU_OTHERS_LAT ),
-       .ZFINX                    ( `ZFINX ),
-       .NUM_MHPMCOUNTERS         ( `NUM_MHPMCOUNTERS )
-   )
-   cv32e40p_core_ip (
-       .clk_i                    (clk_i),
-       .rst_ni                   (rst_ni),
+      .FPU             (0),
+      .FPU_ADDMUL_LAT  (0),
+      .FPU_OTHERS_LAT  (0),
+      .ZFINX           (0),
+      .COREV_PULP      (0),
+      .COREV_CLUSTER   (0),
+      .NUM_MHPMCOUNTERS(1)
+   ) core (
+      // Clock and reset
+      .rst_ni      (rst_ni),
+      .clk_i       (clk_i),
+      .scan_cg_en_i(1'b0),
 
-       .pulp_clock_en_i          (`PULP_CLOCK_EN), // PULP clock enable (only used if COREV_CLUSTER = 1)
-       .scan_cg_en_i             (`SCAN_CG_EN), // Enable all clock gates for testing
+      // Special control signals
+      .fetch_enable_i (1),
+      .pulp_clock_en_i(1'b0),
+      .core_sleep_o   (),
 
-       // Configuration
-       .boot_addr_i              (`BOOT_ADDR),
-       .mtvec_addr_i             (`MTVEC_ADDR),
-       .dm_halt_addr_i           (`DM_HALT_ADDR),
-       .hart_id_i                (`HART_ID),
-       .dm_exception_addr_i      (`DM_EXCEPTION_ADDR),
+      // Configuration
+      .boot_addr_i        (32'h0000_0080),
+      .mtvec_addr_i       (32'h0000_0000),
+      .dm_halt_addr_i     (32'h00000000),
+      .dm_exception_addr_i(32'h00000000),
+      .hart_id_i          (32'b0),
 
-       // Instruction memory interface
-       .instr_req_o              (instr_req),
-       .instr_gnt_i              (instr_gnt),
-       .instr_rvalid_i           (instr_rvalid),
-       .instr_addr_o             (instr_addr),
-       .instr_rdata_i            (instr_rdata),
+      // Instruction memory interface
+      .instr_addr_o  (instr_addr),
+      .instr_req_o   (instr_req),
+      .instr_gnt_i   (instr_gnt),
+      .instr_rvalid_i(instr_rvalid),
+      .instr_rdata_i (instr_rdata),
 
-       // Data memory interface
-       .data_req_o               (data_req),
-       .data_gnt_i               (data_gnt),
-       .data_rvalid_i            (data_rvalid),
-       .data_we_o                (data_we),
-       .data_be_o                (data_be),
-       .data_addr_o              (data_addr),
-       .data_wdata_o             (data_wdata),
-       .data_rdata_i             (data_rdata),
+      // Data memory interface
+      .data_addr_o  (data_addr),
+      .data_req_o   (data_req),
+      .data_gnt_i   (data_gnt),
+      .data_we_o    (data_we),
+      .data_be_o    (data_be),
+      .data_wdata_o (data_wdata),
+      .data_rvalid_i(data_rvalid),
+      .data_rdata_i (data_rdata),
 
-       // TODO: Interrupt instead of polling peripherals
-       // Interrupt interface
-       .irq_i                    (32'h0), //({14'b0, timer_bus.irq, gpio_bus.irq, 16'b0}), //4'b0, 0, 3'b0, 0, 3'b0, 0, 3'b0}),
-       .irq_ack_o                (),
-       .irq_id_o                 (),
+      // Interrupt interface
+      .irq_i                    (0), //({14'b0, timer_bus.irq, gpio_bus.irq, 16'b0}), //4'b0, 0, 3'b0, 0, 3'b0, 0, 3'b0}),
+      .irq_ack_o(),
+      .irq_id_o(),
 
-       // TODO: JTAG Integration
-       // Debug interface
-       .debug_req_i              (1'b0),
-       .debug_havereset_o        (),
-       .debug_running_o          (),
-       .debug_halted_o           (),
-
-       // CPU Control Signals
-       .fetch_enable_i           (1'b1),
-       .core_sleep_o             ()
+      // Debug interface
+      .debug_req_i      (1'b0),
+      .debug_havereset_o(),
+      .debug_running_o  (),
+      .debug_halted_o   ()
    );
 
-   // instruction cache
    localparam int unsigned ICACHE_WAY_LEN = ICACHE_SZ / (ICACHE_LINE_W / 8) / 2;
+
    cache #(
       .ADDR_BIT_W (32),
       .CPU_BYTE_W (4),
@@ -170,6 +202,15 @@ module air_soc #(
    );
 
    // data cache
+   logic               dmem_req;
+   logic               dmem_gnt;
+   logic [       31:0] dmem_addr;
+   logic               dmem_we;
+   logic [MEM_W  -1:0] dmem_wdata;
+   logic               dmem_rvalid;
+   logic               dmem_wvalid;
+   logic [MEM_W  -1:0] dmem_rdata;
+
    localparam int unsigned DCACHE_WAY_LEN = DCACHE_SZ / (DCACHE_LINE_W / 8) / 2;
    cache #(
       .ADDR_BIT_W (32),
@@ -200,8 +241,11 @@ module air_soc #(
       .mem_rdata_i (dmem_rdata)
    );
 
+
+
    ///////////////////////////////////////////////////////////////////////////
    // MEMORY ARBITER
+
    always_comb begin
       mem_req   = imem_req | dmem_req;
       mem_addr  = imem_addr;
@@ -271,36 +315,6 @@ module air_soc #(
       //.prog_mode_led_o(prog_mode_led_o)
    );
 
-   logic [3:0] qspi_miso;
-   logic [3:0] qspi_mosi;
-   logic [3:0] qspi_sck;
-   logic [3:0] qspi_cs;
-   logic [1:0] qspi_out_mod;
-
-   periph_bus pb(
-      .clk_i(clk_i),
-      .rst_i(~rst_ni),
-
-      .req_i   (periph_req),
-      .we_i    (periph_we),
-      .be_i    (periph_be),
-      .addr_i  (periph_addr),
-      .wdata_i (periph_wdata),
-      .gnt_o   (periph_gnt),
-      .rvalid_o(periph_rvalid),
-      .rdata_o (periph_rdata),
-
-      .uart_rx_i      (uart_rx_i),
-      .uart_tx_o      (uart_tx_o),
-
-      .qspi_miso_i    (qspi_miso),
-      .qspi_mosi_o    (qspi_mosi),
-      .qspi_sck_o     (qspi_sck),
-      .qspi_cs_o      (qspi_cs),
-      .qspi_out_mod_o (qspi_out_mod)
-   );
-   
-
    obi_demux obi_demux_dut (
       .clk_i (clk_i),
       .rst_ni(rst_ni),
@@ -323,14 +337,141 @@ module air_soc #(
       .cache_rvalid_i(cache_rvalid),
       .cache_rdata_i (cache_rdata),
 
-      .periph_req_o   (periph_req),
-      .periph_addr_o  (periph_addr),
-      .periph_we_o    (periph_we),
-      .periph_be_o    (periph_be),
-      .periph_wdata_o (periph_wdata),
-      .periph_gnt_i   (periph_gnt),
-      .periph_rvalid_i(periph_rvalid),
-      .periph_rdata_i (periph_rdata)
+      .uart_req_o   (uart_req),
+      .uart_addr_o  (uart_addr),
+      .uart_we_o    (uart_we),
+      .uart_be_o    (uart_be),
+      .uart_wdata_o (uart_wdata),
+      .uart_gnt_i   (uart_gnt),
+      .uart_rvalid_i(uart_rvalid),
+      .uart_rdata_i (uart_rdata),
+
+      .spi_req_o   (spi_req),
+      .spi_addr_o  (spi_addr),
+      .spi_we_o    (spi_we),
+      .spi_be_o    (spi_be),
+      .spi_wdata_o (spi_wdata),
+      .spi_gnt_i   (spi_gnt),
+      .spi_rvalid_i(spi_rvalid),
+      .spi_rdata_i (spi_rdata),
+
+      .timer_req_o   (timer_req),
+      .timer_addr_o  (timer_addr),
+      .timer_we_o    (timer_we),
+      .timer_be_o    (timer_be),
+      .timer_wdata_o (timer_wdata),
+      .timer_gnt_i   (timer_gnt),
+      .timer_rvalid_i(timer_rvalid),
+      .timer_rdata_i (timer_rdata),
+
+      .i2c_req_o   (i2c_req),
+      .i2c_addr_o  (i2c_addr),
+      .i2c_we_o    (i2c_we),
+      .i2c_be_o    (i2c_be),
+      .i2c_wdata_o (i2c_wdata),
+      .i2c_gnt_i   (i2c_gnt),
+      .i2c_rvalid_i(i2c_rvalid),
+      .i2c_rdata_i (i2c_rdata),
+
+      .gpio_req_o   (gpio_req),
+      .gpio_addr_o  (gpio_addr),
+      .gpio_we_o    (gpio_we),
+      .gpio_be_o    (gpio_be),
+      .gpio_wdata_o (gpio_wdata),
+      .gpio_gnt_i   (gpio_gnt),
+      .gpio_rvalid_i(gpio_rvalid),
+      .gpio_rdata_i (gpio_rdata),
+
+      .usb_req_o   (usb_req),
+      .usb_addr_o  (usb_addr),
+      .usb_we_o    (usb_we),
+      .usb_be_o    (usb_be),
+      .usb_wdata_o (usb_wdata),
+      .usb_gnt_i   (usb_gnt),
+      .usb_rvalid_i(usb_rvalid),
+      .usb_rdata_i (usb_rdata)
+
+   );
+
+
+   uart_controller_obi uart (
+      .clk_i   (clk_i),
+      .rst_ni  (rst_ni),
+      .req_i   (uart_req),
+      .we_i    (uart_we),
+      .be_i    (uart_be),
+      .addr_i  (uart_addr),
+      .wdata_i (uart_wdata),
+      .gnt_o   (uart_gnt),
+      .rvalid_o(uart_rvalid),
+      .rdata_o (uart_rdata),
+      .rx_i    (uart_rx_i),
+      .tx_o    (uart_tx_o)
+   );
+
+   timer_controller_obi timer (
+      .clk_i   (clk_i),
+      .rst_ni  (rst_ni),
+      .req_i   (timer_req),
+      .we_i    (timer_we),
+      .be_i    (timer_be),
+      .addr_i  (timer_addr),
+      .wdata_i (timer_wdata),
+      .gnt_o   (timer_gnt),
+      .rvalid_o(timer_rvalid),
+      .rdata_o (timer_rdata)
+   );
+
+   spi_controller_obi spi (
+      .clk_i   (clk_i),
+      .rst_ni  (rst_ni),
+      .req_i   (spi_req),
+      .we_i    (spi_we),
+      .be_i    (spi_be),
+      .addr_i  (spi_addr),
+      .wdata_i (spi_wdata),
+      .gnt_o   (spi_gnt),
+      .rvalid_o(spi_rvalid),
+      .rdata_o (spi_rdata)
+   );
+
+   i2c_controller_obi i2c (
+      .clk_i   (clk_i),
+      .rst_ni  (rst_ni),
+      .req_i   (i2c_req),
+      .we_i    (i2c_we),
+      .be_i    (i2c_be),
+      .addr_i  (i2c_addr),
+      .wdata_i (i2c_wdata),
+      .gnt_o   (i2c_gnt),
+      .rvalid_o(i2c_rvalid),
+      .rdata_o (i2c_rdata)
+   );
+
+   gpio_controller_obi gpio (
+      .clk_i   (clk_i),
+      .rst_ni  (rst_ni),
+      .req_i   (gpio_req),
+      .we_i    (gpio_we),
+      .be_i    (gpio_be),
+      .addr_i  (gpio_addr),
+      .wdata_i (gpio_wdata),
+      .gnt_o   (gpio_gnt),
+      .rvalid_o(gpio_rvalid),
+      .rdata_o (gpio_rdata)
+   );
+
+   usb_controller_obi usb (
+      .clk_i   (clk_i),
+      .rst_ni  (rst_ni),
+      .req_i   (usb_req),
+      .we_i    (usb_we),
+      .be_i    (usb_be),
+      .addr_i  (usb_addr),
+      .wdata_i (usb_wdata),
+      .gnt_o   (usb_gnt),
+      .rvalid_o(usb_rvalid),
+      .rdata_o (usb_rdata)
    );
 
 endmodule
