42448b159db686656515bb8fc637c6bcb775b073
celuk <seyyid4091@gmail.com>
2024-08-22 00:40:07 +0300

    fix: set relative path for flash mem file

--- rtl/src/soc/air_soc.sv
+++ rtl/src/soc/air_soc.sv
@@ -481,7 +481,7 @@ module air_soc (
 
 `ifdef QSPI_SIM
    s25fl128s #(
-      .mem_file_name("s25fl128s.mem"),
+      .mem_file_name("../../../rtl/sim/s25fl128s.mem"),
       .otp_file_name("none"),
       .AddrRANGE(24'h000FFF)
    ) flash (
