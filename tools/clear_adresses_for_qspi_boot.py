f1d8ad26545b7c4ef45ce0faa8e8bb450d68604d
celuk <seyyid4091@gmail.com>
2024-09-05 07:14:26 +0300

    fix: gap count for vmem

--- tools/clear_adresses_for_qspi_boot.py
+++ tools/clear_adresses_for_qspi_boot.py
@@ -6,18 +6,21 @@ def process_vmem_file(input_file, place_zero):
 
     filtered_lines = []
     previous_address = None
+    previous_data_length = 0
 
     for line in lines:
         if line.startswith('@'):
             current_address = int(line[1:], 16)
             if previous_address is not None and place_zero:
-                gap = current_address - previous_address
+                gap = current_address - (previous_address + previous_data_length)
                 if gap > 0:
                     zero_bytes = '00 ' * gap
                     filtered_lines.append(zero_bytes.strip() + '\n')
             previous_address = current_address
+            previous_data_length = 0
         else:
             filtered_lines.append(line)
+            previous_data_length += len(line.split())
 
     # Add @00000000 at the top
     filtered_lines.insert(0, '@00000000\n')
