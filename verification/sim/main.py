84ed565c03b35cce1fc37bfd66576e2566bf7b08
celuk <seyyid4091@gmail.com>
2025-05-20 09:36:19 +0300

    use pre input for xrun

--- verification/sim/main.py
+++ verification/sim/main.py
@@ -68,63 +68,65 @@ def run_test(simulator: str, test_file: Path, top_module: str, waves: bool, cfil
     print("\nVERILOG_SOURCES:")
     print(verilog_sources)
 
-    import cocotb
-    from cocotb.runner import Xcelium
-    def fixed_test_command(self):
-        self.env["CDS_AUTO_64BIT"] = "all"
-
-        if self.pre_cmd:
-            print("WARNING: pre_cmd is not implemented for Xcelium.")
-
-        verbosity_opts = []
-        if self.verbose:
-            verbosity_opts += ["-messages", "-status", "-gverbose", "-pliverbose", "-plidebug", "-plierr_verbose"]
-        else:
-            verbosity_opts += ["-quiet", "-plinowarn"]
-
-        tmpdir = f"implicit_tmpdir_{self.current_test_name}"
-        xrun_top = ":" if self.hdl_toplevel_lang == "vhdl" else self.sim_hdl_toplevel
-
-        input_script = (
-            f"@database -open cocotb_waves -default;"
-            f"probe -database cocotb_waves -create {xrun_top} -all -depth all;"
-        #    f"probe -create -packed 131072 *;"
-            f"run;"
-            f"exit;"
-            if self.waves
-            else "@run; exit;"
-        )
-
-        cmds = [["mkdir", "-p", tmpdir]]
-        cmds += [
-            ["xrun"]
-            + ["-logfile", f"xrun_{self.current_test_name}.log"]
-            + ["-xmlibdirname", f"{self.build_dir}/xrun_snapshot"]
-            + ["-cds_implicit_tmpdir", tmpdir]
-            + ["-licqueue"]
-            + verbosity_opts
-            + ["-R"]
-            + self.test_args
-            + self.plusargs
-            + (["-gui"] if self.gui else [])
-            + ["-input", input_script]
-        ]
-
-        self.env["GPI_EXTRA"] = (
-            cocotb.config.lib_name_path("vhpi", "xcelium") + ":cocotbvhpi_entry_point"
-        )
-
-        return cmds
-    Xcelium._test_command = fixed_test_command
+    #import cocotb
+    #from cocotb.runner import Xcelium
+    #def fixed_test_command(self):
+    #    self.env["CDS_AUTO_64BIT"] = "all"
+#
+    #    if self.pre_cmd:
+    #        print("WARNING: pre_cmd is not implemented for Xcelium.")
+#
+    #    verbosity_opts = []
+    #    if self.verbose:
+    #        verbosity_opts += ["-messages", "-status", "-gverbose", "-pliverbose", "-plidebug", "-plierr_verbose"]
+    #    else:
+    #        verbosity_opts += ["-quiet", "-plinowarn"]
+#
+    #    tmpdir = f"implicit_tmpdir_{self.current_test_name}"
+    #    xrun_top = ":" if self.hdl_toplevel_lang == "vhdl" else self.sim_hdl_toplevel
+#
+    #    input_script = (
+    #        f"@database -open cocotb_waves -default;"
+    #        f"probe -database cocotb_waves -create {xrun_top} -all -depth all;"
+    #    #    f"probe -create -packed 131072 *;"
+    #        f"run;"
+    #        f"exit;"
+    #        if self.waves
+    #        else "@run; exit;"
+    #    )
+#
+    #    cmds = [["mkdir", "-p", tmpdir]]
+    #    cmds += [
+    #        ["xrun"]
+    #        + ["-logfile", f"xrun_{self.current_test_name}.log"]
+    #        + ["-xmlibdirname", f"{self.build_dir}/xrun_snapshot"]
+    #        + ["-cds_implicit_tmpdir", tmpdir]
+    #        + ["-licqueue"]
+    #        + verbosity_opts
+    #        + ["-R"]
+    #        + self.test_args
+    #        + self.plusargs
+    #        + (["-gui"] if self.gui else [])
+    #        + ["-input", input_script]
+    #    ]
+#
+    #    self.env["GPI_EXTRA"] = (
+    #        cocotb.config.lib_name_path("vhpi", "xcelium") + ":cocotbvhpi_entry_point"
+    #    )
+#
+    #    return cmds
+    #Xcelium._test_command = fixed_test_command
 
     runner_build_args = ["-modelsimini", "../../../vivado/modelsim.ini"]
     runner_pre_cmd = ['set WildcardFilter {};set WildcardSizeThreshold "16777216"; coverage save -onexit covres.ucdb;']
-    runner_test_args = ["-suppress", "16154", "-suppress", "8630", "-modelsimini", "../../../vivado/modelsim.ini", "-L", "compiled-libs", "top.glbl"]
+    runner_test_args = ["-suppress", "14408", "-suppress", "16154", "-suppress", "8630", "-modelsimini", "../../../vivado/modelsim.ini", "-L", "compiled-libs", "top.glbl"]
 
     if simulator.lower() == "xcelium":
-        runner_build_args = ["-verbose", "-access", "+rwc", "-timescale", "1ns/1ps", "-ALLOWREDEFINITION", "-relax", "-sv", "-NEW_VHPI_PROPAGATE_DELAY"]
+        with open("pre_input.tcl", "w") as f:
+            f.writelines(["set probe_packed_limit 0;\n", "set probe_unpacked_limit 0;\n"])
+        runner_build_args = ["-verbose", "-access", "+rwc", "-timescale", "1ns/1ps", "-ALLOWREDEFINITION", "-relax", "-sv"]
         runner_pre_cmd = []
-        runner_test_args = [] #["set probe_packed_limit 131072; set probe_unpacked_limit 131072;"] #["probe -create -packed 131072 *;"]
+        runner_test_args = ["-pre_input", "../pre_input.tcl"] #["set probe_packed_limit 131072; set probe_unpacked_limit 131072;"] #["probe -create -packed 131072 *;"]
 
     runner = get_runner(simulator)
     runner.build(
@@ -150,8 +152,8 @@ def run_test(simulator: str, test_file: Path, top_module: str, waves: bool, cfil
         #    "COCOTB_LOG_LEVEL": "TRACE",
         #    "COCOTB_SCHEDULER_DEBUG": "1",
             "SHM_RESET_DEFAULTS": "1",
-            "SHM_UNPACKED_LIMIT": "131072",
-            "SHM_PACKED_LIMIT": "131072",
+        #    "SHM_UNPACKED_LIMIT": "131072",
+        #    "SHM_PACKED_LIMIT": "131072",
             "COCOTB_HDL_TIMEUNIT": "1ns",
             "COCOTB_HDL_TIMEPRECISION": "1ps",
             "CFILE": cfile,
