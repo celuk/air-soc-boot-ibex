b03d7598a8d386bbecacffa15da2c5261273090a
celuk <seyyid4091@gmail.com>
2026-05-03 15:10:54 +0300

    try aes256 gcm

--- rtl/src/soc/air_soc.sv
+++ rtl/src/soc/air_soc.sv
@@ -823,9 +823,9 @@ module air_soc (
    wire [3:0] qspi_data_io;
 
    s25fl128s #(
-      .mem_file_name("../../../tests/coremark/coremark_baremetal2.vmem"),
+      //.mem_file_name("../../../tests/coremark/coremark_baremetal2.vmem"),
       //.mem_file_name("../../../tests/demo/demo.vmem"),
-      //.mem_file_name("../../../tests/demo/demo_secure.vmem"),
+      .mem_file_name("../../../tests/demo/demo_secure.vmem"),
       //.mem_file_name("../../../rtl/sim/s25fl128s.mem"),
       //.mem_file_name("none"),
       .otp_file_name("none"),
